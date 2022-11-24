22f5bf6549c586b8d2580f83163d21ed4a43aa47
Janik Schumacher <rndmb0t@gmail.com>
2022-11-24 22:20:19 +0000

    first server call 🎉

--- src/Client/web/src/app/common/feed-list/feed-list.component.ts
+++ src/Client/web/src/app/common/feed-list/feed-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeedsService } from '../services/api/services';
 
 @Component({
   selector: 'rtfx-feed-list',
   templateUrl: './feed-list.component.html',
   styleUrls: ['./feed-list.component.scss']
 })
-export class FeedListComponent {}
+export class FeedListComponent implements OnInit {
+  private readonly _feedsService: FeedsService;
+
+  constructor(feedsService: FeedsService) {
+    this._feedsService = feedsService;
+  }
+
+  ngOnInit(): void {
+    this._feedsService.listFeedsEndpoint().then(feeds => {
+      console.log(feeds);
+    });
+  }
+}
